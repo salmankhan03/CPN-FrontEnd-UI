f4a05091b102e0aa2f886734d327311699c5fe36
Usman Kapadiya <hirenk@intellidt.com>
2024-05-30 10:53:14 +0530

    loader set

--- src/pages/PrivacyPolicy/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy/PrivacyPolicy.js
@@ -2,13 +2,23 @@
 import React, { useEffect, useState } from 'react';
 import FooterComponents from '../../components/FooterComponents/FooterComponents';
 import { useSelector } from 'react-redux';
+import SpinnerLoading from '../../components/SpinnerComponents/SpinnerLoader';
+
 const PrivacyPolicy = () => {
+    const [loading, setLoading] = useState(true)
     const templateList = useSelector(state => state.TemplateReducer.templateList);
-    const [privacyPolicyData, setPrivacyPolicyData]= useState()
-    useEffect(()=>{
-        let data= templateList?.find((x)=>x?.name === "Privacy policy")
-       setPrivacyPolicyData(data?.template)
-    },[templateList])
+    const [privacyPolicyData, setPrivacyPolicyData] = useState()
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            const data = templateList?.find(x => x?.name === "Privacy policy");
+            setPrivacyPolicyData(data?.template || '');
+            setLoading(false);
+        }, 500);
+        return () => clearTimeout(timer);
+    }, [templateList])
+    if (loading) {
+        return <SpinnerLoading loading={loading} />;
+    }
     return (
         <div className=''>
             <div className='custom-container'>
--- src/pages/Terms/Terms.js
+++ src/pages/Terms/Terms.js
@@ -2,13 +2,23 @@
 import React, { useEffect, useState } from 'react';
 import FooterComponents from '../../components/FooterComponents/FooterComponents';
 import { useSelector } from 'react-redux';
+import SpinnerLoading from '../../components/SpinnerComponents/SpinnerLoader';
 const TermsAndCondition = () => {
+    const [loading, setLoading] = useState(true)
     const templateList = useSelector(state => state.TemplateReducer.templateList);
     const [content, setContent]= useState()
     useEffect(()=>{
-        let data= templateList?.find((x)=>x?.name === "TERMS & CONDITIONS")
-        setContent(data?.template)
+        const timer = setTimeout(() => {
+            const data = templateList?.find(x => x?.name === "TERMS & CONDITIONS");
+            setContent(data?.template || '');
+            setLoading(false); 
+        }, 500); 
+        return () => clearTimeout(timer);
+      
     },[templateList])
+    if (loading) {
+        return <SpinnerLoading loading={loading} />;
+    }
     return (
         <div className=''>
             <div className='custom-container'>
