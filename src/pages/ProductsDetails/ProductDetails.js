5d664db3b95b7fb33869961d3d90d520106dd690
Usman Kapadiya <hirenk@intellidt.com>
2024-06-11 16:45:15 +0530

    products description text align left set

--- src/pages/ProductsDetails/ProductDetails.js
+++ src/pages/ProductsDetails/ProductDetails.js
@@ -713,7 +713,7 @@ function ProductDetails() {
                             <Container className="mt-4">
                                 <Tabs defaultActiveKey="description" id="tab-component" className="custom-tabs">
                                     <Tab eventKey="description" title="Description">
-                                        <div className="tab-content-custom">
+                                        <div className="tab-content-custom text-left">
                                             <div dangerouslySetInnerHTML={{ __html: decodeURIComponent((productData?.description === null) ? "" : productData?.description) }} />;
                                         </div>
                                     </Tab>
