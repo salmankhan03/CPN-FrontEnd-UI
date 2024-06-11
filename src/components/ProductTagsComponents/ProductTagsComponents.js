ca81fc6195bb10f724782feefe2ecedd84c2ea22
Usman Kapadiya <hirenk@intellidt.com>
2024-06-11 15:20:34 +0530

    update tags component

--- src/components/ProductTagsComponents/ProductTagsComponents.js
+++ src/components/ProductTagsComponents/ProductTagsComponents.js
@@ -3,11 +3,13 @@ import React from "react";
 function ProductTags({ tags }) {
     return (
       <div className="product-tags-container">
-        {tags?.map((tag, index) => (
+        {tags?.map((tag, index) =>{
+          console.log("tag",tag)
+          return (
           <span key={index} className="product-tag">
-            {tag}
+            {tag?.name}
           </span>
-        ))}
+        )})}
       </div>
     );
   }
