9899118ebb04aed12ffbb26034545c0a4d8b794a
Usman Kapadiya <hirenk@intellidt.com>
2024-07-11 17:52:03 +0530

    disabled button property set

--- src/components/ButtonComponents/ButtonComponents.js
+++ src/components/ButtonComponents/ButtonComponents.js
@@ -1,7 +1,11 @@
 import React from "react"
-function ButtonComponent ({ onClick,cssClass, label }) {
+function ButtonComponent ({ onClick,cssClass, label,disabled }) {
     return(
-        <button className={`${cssClass ? cssClass :''} red_button product-add_to_cart_button1`} onClick={onClick}>
+        <button 
+            className={`${cssClass ? cssClass :''} red_button product-add_to_cart_button1`} 
+            onClick={onClick}
+            disabled={disabled}
+        >
             {label}
         </button>
 )
