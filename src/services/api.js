e1a7bc231df2708aba5560143d4053cfddcd8284
Usman Kapadiya <hirenk@intellidt.com>
2024-07-24 15:13:40 +0530

    base url update

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const instance = axios.create({
-  baseURL: ` https://backend.kingsmankids.com/api`,
+  baseURL: `https://backend.i-healthcare.ca/api`,
   timeout: 50000,
   headers: {
     Accept: 'application/json',
