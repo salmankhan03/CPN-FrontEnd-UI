30282b2d92eed4ee023f5a7e57b053c9451cd8e5
smit-intellidt <139744964+smit-intellidt@users.noreply.github.com>
2024-07-25 16:31:03 +0530

    changed the base url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const instance = axios.create({
-  baseURL: `http://backend.i-healthcare.ca/api`,
+  baseURL: `https://backend.i-healthcare.ca/api`,
   timeout: 50000,
   headers: {
     Accept: 'application/json',
