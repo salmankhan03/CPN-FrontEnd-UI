96aa27861e3db77397c8e192fae5ca94937f7f07
Usman Kapadiya <hirenk@intellidt.com>
2024-08-02 15:39:57 +0530

    Thank You screen in Header set and Button Component set

--- src/pages/ThankYou/ThankYou.js
+++ src/pages/ThankYou/ThankYou.js
@@ -3,6 +3,8 @@ import ImageComponent from '../../components/ImageComponents/ImageComponents';
 import thankyou from "../../assets/images/tutorial-preview-large.png"
 import { useNavigate } from 'react-router-dom';
 import FooterComponents from '../../components/FooterComponents/FooterComponents';
+import Header from '../../components/HeaderComponents/HeaderComponents';
+import ButtonComponent from '../../components/ButtonComponents/ButtonComponents';
 const ThankYouScreen = () => {
   const navigate = useNavigate();
   const [orderId, setOrderId] = useState('sadsafrewtretfsd21454');
@@ -15,17 +17,20 @@ const ThankYouScreen = () => {
     navigate(`/`)
   }
   return (
-    <div className="container mt-5">
-      <div className='text-center  mt-5 mb-5' >
-        <ImageComponent src={thankyou} alt="Success" classAtribute="imageHeight" />
-        <h1 className='mt-1'>Order Successful</h1>
-        <p>Your Order ID: {orderId}</p>
-        <button type="submit" className="place_order" onClick={gotoHome}>Back to Home</button>
+    <>
+      <Header />
+      <div className="container mt-5">
+        <div className='text-center  mt-5 mb-5' >
+          <ImageComponent src={thankyou} alt="Success" classAtribute="imageHeight" />
+          <h1 className='mt-1'>Order Successful</h1>
+          <p>Your Order ID: {orderId}</p>
+           <ButtonComponent cssClass="shopping-btn btn-border-radius mt-3" onClick={gotoHome} label="Back to Home" />
+        </div>
+        <div className='pb-2'>
+          <FooterComponents />
+        </div>
       </div>
-      <div className='pb-2'>
-        <FooterComponents />
-      </div>
-    </div>
+    </>
   );
 };
 
