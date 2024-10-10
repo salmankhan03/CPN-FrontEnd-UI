b6e90ce2e9b74ec5635890c67d70993672e75ead
Usman Kapadiya <hirenk@intellidt.com>
2024-10-10 17:21:26 +0530

    mega menu bugs fixed

--- src/components/MegaMenuComponents/MegaMenuComponents.js
+++ src/components/MegaMenuComponents/MegaMenuComponents.js
@@ -2,34 +2,46 @@ import React from 'react';
 import './MegaMenu.css';
 import { useNavigate } from 'react-router-dom';
 
-const RenderCategories = ({ categories, type, isTopLevel = true, onCategoryClick }) => {
+const RenderCategories = ({ categories, type, level = 0, onCategoryClick }) => {
   const navigate = useNavigate();
 
-  const handleCategoryClick = (category,type) => {
+  const handleCategoryClick = (category, type) => {
     if (onCategoryClick) {
-       onCategoryClick(category,type);
+      onCategoryClick(category, type);
     }
     if (type === 'brand') {
-      navigate(`/shop?name=brand&id=${category?.id}`)
+      navigate(`/shop?name=brand&id=${category?.id}`);
     } else {
       navigate(`/shop?name=category&id=${category.id}`, { state: { selectedCategory: category.id } });
     }
   };
 
-  return categories.map((category) => (
-    <div className="column col-md-3" key={category.id}>
-      {type === 'brand' ? (
-        <p className='text-black' onClick={() => handleCategoryClick(category,type)}>{category.name}</p>
-      ) : (
-        isTopLevel ? <strong className='text-black' onClick={() => handleCategoryClick(category,type)}>{category.name}</strong> : <p className='text-black' onClick={() => handleCategoryClick(category,type)}>{category.name}</p>
-      )}
-      {category.children && category.children.length > 0 && (
-        <div className="children">
-          <RenderCategories categories={category.children} type={type} isTopLevel={false} onCategoryClick={onCategoryClick} />
+  return (
+    <>
+      {categories.map((category) => (
+        <div className={`category-item ${level === 0 ? 'col-md-3' : ''}`} key={category.id}>
+          <div>
+             {/* style={{ paddingLeft: `${level * }px` }}> */}
+    
+            {/* Category Name */}
+            <span
+              className={`category-name ${level === 0 ? 'font-weight-bold' : ''}`}
+              onClick={() => handleCategoryClick(category, type)}
+            >
+              {category.name}
+            </span>
+
+            {/* Render Children Categories */}
+            {category.children && category.children.length > 0 && (
+              <div className="child-categories">
+                <RenderCategories categories={category.children} type={type} level={level + 1} onCategoryClick={onCategoryClick} />
+              </div>
+            )}
+          </div>
         </div>
-      )}
-    </div>
-  ));
+      ))}
+    </>
+  );
 };
 
 const MegaMenu = ({ isOpen, data, type, onMouseEnter, onMouseLeave }) => {
@@ -37,7 +49,7 @@ const MegaMenu = ({ isOpen, data, type, onMouseEnter, onMouseLeave }) => {
     <div className={`mega-menu ${isOpen ? 'open' : ''}`} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       <div className="mega-menu-content container">
         <div className="row">
-          {data && <RenderCategories categories={data} type={type} isTopLevel={true} />}
+          {data && <RenderCategories categories={data} type={type} />}
         </div>
       </div>
     </div>
